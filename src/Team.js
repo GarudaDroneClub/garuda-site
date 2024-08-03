7540f075502f928ca9b4c8849e9043b7cfd0a540
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-08-03 21:28:40 +0530

    Update Team.js

--- src/Team.js
+++ src/Team.js
@@ -2,24 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
 import './Team.css';
-import TeamVideo from './TeamVideo.mp4';
+
 
 const Team = () => {
-  console.log('Video source:', TeamVideo); // Debugging line
   return (
     <div className="team-container">
       <Header />
-      <video autoPlay loop muted className="team-video">
-        <source src={TeamVideo} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
       <header className="team-header">
         <h2>Select Year</h2>
       </header>
       <div className="team-year-sections">
-        <Link to="/team/fi" className="team-section">
+      <Link to="/team/fi" className="team-section">
           <h2>Faculty Incharge</h2>
         </Link>
+        
+        
         <Link to="/team/y22" className="team-section">
           <h2>Y22</h2>
         </Link>
