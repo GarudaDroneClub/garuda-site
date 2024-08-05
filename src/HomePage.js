f68f5afb51593cc9cbc1dcdeca0cdfb82bc667bc
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-08-05 15:44:10 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -11,31 +11,32 @@ const HomePage = () => {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <header className="home-header">
-        <h1>Welcome to KL- GARUDA Drone Technology Club</h1>
-        <p>Innovating the future of drones and technology.</p>
-      </header>
-      <section className="home-content">
-        <a href="/about" className="home-section">
-          <h2>About Us</h2>
-        </a>
-        <a href="/ourjourney" className="home-section">
-          <h2>Our Journey</h2>
-        </a>
-        <a href="/upcoming-events" className="home-section">
-          <h2>Upcoming Events</h2>
-        </a>
-        <a href="/past-events" className="home-section">
-          <h2>Past Events</h2>
-        </a>
-        <a href="/team" className="home-section">
-          <h2>Team</h2>
-        </a>
-        
-        <a href="/contact" className="home-section">
-          <h2>Contact Us</h2>
-        </a>
-      </section>
+      <div className="home-shadow-box">
+        <header className="home-header">
+          <h1>Welcome to KL- GARUDA Drone Technology Club</h1>
+          <p>Innovating the future of drones and technology.</p>
+        </header>
+        <section className="home-content">
+          <a href="/about" className="home-section">
+            <h2>About Us</h2>
+          </a>
+          <a href="/ourjourney" className="home-section">
+            <h2>Our Journey</h2>
+          </a>
+          <a href="/upcoming-events" className="home-section">
+            <h2>Upcoming Events</h2>
+          </a>
+          <a href="/past-events" className="home-section">
+            <h2>Past Events</h2>
+          </a>
+          <a href="/team" className="home-section">
+            <h2>Team</h2>
+          </a>
+          <a href="/contact" className="home-section">
+            <h2>Contact Us</h2>
+          </a>
+        </section>
+      </div>
       <footer className="home-footer">
         <p>Developed by G. Venkat</p>
       </footer>
