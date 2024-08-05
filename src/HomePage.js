6fb8f8b059c235eee84a8d50d36a82caf46dd637
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-08-05 15:19:20 +0530

    Update HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -11,10 +11,11 @@ const HomePage = () => {
         <source src={backgroundVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-          <section className="home-content">
-
+      <header className="home-header">
         <h1>Welcome to KL- GARUDA Drone Technology Club</h1>
         <p>Innovating the future of drones and technology.</p>
+      </header>
+      <section className="home-content">
         <a href="/about" className="home-section">
           <h2>About Us</h2>
         </a>
