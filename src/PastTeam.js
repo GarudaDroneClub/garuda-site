65e241e7bf84a62f96044c32b38df89bf11518a4
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-10-04 18:25:08 +0530

    Create PastTeam.js

---
+++ src/PastTeam.js
@@ -0,0 +1,42 @@
+// PastTeam.js
+import React from 'react';
+import Header from './Header';
+import './Team.css';
+import teamPhoto21 from './teamPhoto21.jpg';
+// ...import other photos
+
+const PastTeam = () => {
+  const pastTeamMembers = [
+    {
+      name: 'A. John',
+      role: 'Former President',
+      photo: teamPhoto21,
+    },
+    {
+      name: 'B. Sarah',
+      role: 'Former Vice President',
+      photo: teamPhoto21,
+    },
+    // ... add more past team members
+  ];
+
+  return (
+    <div className="team-container">
+      <Header />
+      <header className="team-header">
+        <h2>Meet Our Past Team Members</h2>
+      </header>
+      <div className="team-content">
+        {pastTeamMembers.map((member, index) => (
+          <div className="team-member" key={index}>
+            <img src={member.photo} alt={member.name} className="team-photo" />
+            <h3 className="team-name">{member.name}</h3>
+            <p className="team-role">{member.role}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PastTeam;
