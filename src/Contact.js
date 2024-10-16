925a9cf89d87d70d5fe071b7ecd3194a9968a629
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-10-16 12:04:51 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -14,6 +14,7 @@ const Contact = () => {
 
         <p>Email: <a href="mailto:garuda.techclub@kluniversity.in">garuda.techclub@kluniversity.in</a></p>
         <p>Telegram IDs for Direct Contact:</p>
+          <p>Garuda Support Bot - <a href="https://t.me/klgarudabot" target="_blank" rel="noopener noreferrer">https://https://t.me/klgarudabot</a></p>
         <p>President (G. Venkat) - <a href="https://t.me/venkatchowdary6666" target="_blank" rel="noopener noreferrer">https://t.me/venkatchowdary6666</a></p>
         <p>Vice President (Ch. Nithin) - <a href="https://t.me/Nithin1738E" target="_blank" rel="noopener noreferrer">https://t.me/Nithin1738E</a></p>
       </div>
