b6483f732dde6fa09a3413885ff7967e67a6a4cd
GarudaDroneClub <garuda.techclub@kluniversity.in>
2024-11-16 17:01:25 +0530

    Update Team.js

--- src/Team.js
+++ src/Team.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Header from './Header';
 import './Team.css';
 
-
 const Team = () => {
   return (
     <div className="team-container">
@@ -12,19 +11,21 @@ const Team = () => {
         <h2>Select Year</h2>
       </header>
       <div className="team-year-sections">
-      <Link to="/team/fi" className="team-section">
+        <Link to="/team/fi" className="team-section">
           <h2>Faculty Incharge</h2>
         </Link>
-        
-        
+
+        <Link to="/team/2024-2025" className="team-section">
+          <h2>2024-2025</h2>
+        </Link>
+
         <Link to="/team/y22" className="team-section">
           <h2>2023-2024</h2>
-    </Link>
-    
+        </Link>
+
         <Link to="/team/y20" className="team-section">
           <h2>2022-2023 [ALUMNI]</h2>
         </Link>
-
       </div>
     </div>
   );
