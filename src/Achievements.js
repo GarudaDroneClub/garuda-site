baff98faec9e9e41f67df252a7fb6f0232ad8df8
GarudaDroneClub <garuda.techclub@kluniversity.in>
2025-02-19 11:48:42 +0530

    Update Achievements.js

--- src/Achievements.js
+++ src/Achievements.js
@@ -7,6 +7,8 @@ import AchievementPhoto2 from './AchievementPhoto2.jpg';
 import AchievementPhoto3 from './AchievementPhoto3.jpg';
 import AchievementPhoto4 from './AchievementPhoto4.jpg';
 import AchievementPhoto5 from './AchievementPhoto5.jpg';
+import AchievementPhoto5 from './AchievementPhoto6.jpg';
+import AchievementPhoto5 from './AchievementPhoto7.jpg';
 
 const Achievements = () => {
   // Array of achievements with photos and descriptions
@@ -24,6 +26,11 @@ const Achievements = () => {
                   text: "Dear participants, as you embark on Round 2 of the Mahakumbh Hackathon 2025, I extend my heartfelt wishes for a journey filled with innovation, collaboration, and success. Your dedication and creativity have brought you to this stage, and I have no doubt that you will continue to showcase your exceptional talents. Embrace the challenges, learn from every moment, and let your passion drive you to new heights. May this round be a testament to your hard work and determination. Best wishes for an enriching and rewarding experience in Round 2 of the Mahakumbh Hackathon 2025! May your ideas flourish and your solutions inspire. Go team!"
 
     },
+    {
+      image: AchievementPhoto6,
+      image: AchievementPhoto7,
+      text: "We are excited to share that we made a remarkable impact at the Amaravati Drone Summit 2024, showcasing our cutting-edge innovations in drone technology! Our participation in this prestigious event reinforced our commitment to pushing the boundaries of aerial advancements and smart solutions. Competing alongside some of the brightest minds, we demonstrated groundbreaking ideas that captured attention and appreciation. A heartfelt thank you to everyone who supported us on this journey. Stay tuned as we continue to redefine possibilities in the world of drones and technology!"
+},
       {
       image: AchievementPhoto4,
                   text: "We are thrilled to announce that our team has emerged victorious in the zonal round of the prestigious Eureka Pitchathon! This milestone reflects our innovative ideas, hard work, and determination to excel. Competing against some of the brightest minds, we presented a compelling vision that resonated with the judges and stood out as a winning concept."
